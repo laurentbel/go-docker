09d87e0e5928ffea431d794279c7b77f136fcd37
Laurent Bel <bel.laurent@gmail.com>
2022-08-15 12:10:14 +0200

    Adjusting docker tags

--- build-all.sh
+++ build-all.sh
@@ -1,10 +1,10 @@
 echo "Building all images"
-docker build -t go-docker-debian -f ./docker/debian/Dockerfile .
-docker build -t go-docker-alpine -f ./docker/alpine/Dockerfile .
-docker build -t go-docker-distroless -f ./docker/distroless/Dockerfile .
-docker build -t go-docker-busybox -f ./docker/busybox/Dockerfile .
-docker build -t go-docker-scratch -f ./docker/scratch/Dockerfile .
-docker build -t go-docker-scratch-advanced -f ./docker/scratch-advanced/Dockerfile .
+docker build -t go-docker:debian -f ./docker/debian/Dockerfile .
+docker build -t go-docker:alpine -f ./docker/alpine/Dockerfile .
+docker build -t go-docker:distroless -f ./docker/distroless/Dockerfile .
+docker build -t go-docker:busybox -f ./docker/busybox/Dockerfile .
+docker build -t go-docker:scratch -f ./docker/scratch/Dockerfile .
+docker build -t go-docker:scratch-advanced -f ./docker/scratch-advanced/Dockerfile .
 echo "Images built"
 
 echo "Display size"
